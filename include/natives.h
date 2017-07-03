6a4f2d77a94a9d1c8877099638e2169760d1b757
Rock4259 <rock4259@hotmail.com>
2017-07-02 22:39:29 -0400

    Fixed preprocessor error relating to natives.h on GTAIV PC

--- include/natives.h
+++ include/natives.h
@@ -58,10 +58,10 @@
 
 #elif TARGET == TARGET_GTAIV
 
-#if PLATFORM == PLATFORM_X360 || PLATFORM_PS3
-#include "GTAIV/natives32Old.h"//1.0.4.0
-#else
+#if PLATFORM == PLATFORM_PC
 #include "GTAIV/natives32.h"//1.0.7.0
+#else
+#include "GTAIV/natives32Old.h"//1.0.4.0
 #endif
 
 #endif
