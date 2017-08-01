46a6cd673f9a787b6001e104762f0cdf35cd9a77
Rock4259 <rock4259@hotmail.com>
2017-08-01 15:54:23 -0400

    Added 10 more params to overload define

--- include/varargs.h
+++ include/varargs.h
@@ -145,8 +145,8 @@ typedef int* va_list;
 
 #define __NARG__(...)  __NARG_I_(__VA_ARGS__,__RSEQ_N())
 #define __NARG_I_(...) __ARG_N(__VA_ARGS__)
-#define __ARG_N(_1, _2, _3, _4,_5,_6, _7, _8, _9, _10, N ,...) N
-#define __RSEQ_N() 10,9,8,7,6,5,4,3,2,1,0
+#define __ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, N ,...) N
+#define __RSEQ_N() 20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0
 #define _OVERLOAD_(name, n) name##n
 #define _OVERLOAD(name, n) _OVERLOAD_(name, n)
 #define OVERLOAD(func, ...) _OVERLOAD(func, __NARG__(__VA_ARGS__)) (__VA_ARGS__)
